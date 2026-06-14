241ac05546387c7ce2268ed8f0f524b1df657a09
Thrust Vector Labs <thrustvectorlabs@protonmail.com>
2026-06-14 20:08:23 +0200

    Updated script

--- tampermonkey-script/script.js
+++ tampermonkey-script/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FS Economy Tools
 // @namespace    http://tampermonkey.net/
-// @version      0.7.1
+// @version      0.7.2
 // @description  Unofficial FSEconomy tooling
 // @match        https://server.fseconomy.net/*
 // @author       github.com/thrustvectorlabs
