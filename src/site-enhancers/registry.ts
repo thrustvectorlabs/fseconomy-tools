224409d2a20d696c7eeadb38d1953704aa8bb8a1
Thrust Vector Labs <thrustvectorlabs@protonmail.com>
2026-06-14 12:32:25 +0200

    Removed aircraftlog enhancer

--- src/site-enhancers/registry.ts
+++ src/site-enhancers/registry.ts
@@ -1,10 +1,8 @@
-import { aircraftLogEnhancer } from './aircraftlog';
 import { airportEnhancer } from './airport';
 import { myFlightEnhancer } from './myflight';
 import { SiteEnhancerDefinition } from './types';
 
 export const siteEnhancers: SiteEnhancerDefinition[] = [
-  aircraftLogEnhancer,
   airportEnhancer,
   myFlightEnhancer,
 ];
